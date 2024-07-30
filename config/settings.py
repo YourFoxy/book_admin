fb472dbaa5c9fce8cd76ec216a2a6a89750dc907
it <it@lib.local>
2024-07-30 16:40:49 +0300

    3 commit

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STORAGES = {
     "default": {
